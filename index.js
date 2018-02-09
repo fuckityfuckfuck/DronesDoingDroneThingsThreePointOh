8c1f515aa8d14b7e625ba8b7b059c0262507a5bd
Travis Valenti <ennuuos@gmail.com>
2018-02-09 23:27:23 +1100

    Use log option when constructing Drones object
    This loads from the config. In theory all config loading should be done in the index.js instead of in individual files, this stops the config file from being loaded multiple times.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let server = require('./server')
 const Drones = require('./drones')
-let drones = new Drones()
+const config = require('./config.json')
+let drones = new Drones({
+  log: config.log
+})
 
 // This is like this so we don't have to put server in the drone's namespace
 drones.bindServerRoutes(server)
